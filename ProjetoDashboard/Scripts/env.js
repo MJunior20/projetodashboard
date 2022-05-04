42e2ade5aa6cdf3277b5c2d6b0520fc82625e653
Maurilio Junior <mauriliojunior20@hotmail.com>
2022-05-04 13:49:48 -0300

    .

---
+++ ProjetoDashboard/Scripts/env.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBqXZZ1vT2YmUSfSrRUoJWLOMGffpvKNkk",
+    authDomain: "projetodashboard-12d3f.firebaseapp.com",
+    projectId: "projetodashboard-12d3f",
+    storageBucket: "projetodashboard-12d3f.appspot.com",
+    messagingSenderId: "845497449788",
+    appId: "1:845497449788:web:10c22b08db3e221b2cf239",
+    measurementId: "G-LQZP1B9M0G"
+  };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
