41f546a370dcb4f817e042f604ade0b4cc0b9576
Maurilio Junior <mauriliojunior20@hotmail.com>
2022-06-20 17:21:43 -0300

    .

--- src/App.js
+++ src/App.js
@@ -21,53 +21,6 @@ import React from 'react';
 function App() {
   
   
-/*useEffect (async () => {
-  const acessos = collection(db, 'acessosPorEstados');
-  const citySnapshot = await getDocs(acessos);
-  citySnapshot.docs.map(doc => console.log(doc.data()));
-  //db.collection("acessosPorEstados").getDocs((doc) => console.log(doc.data()));
-  
-},[])*/
-  const [listaAcessosEstados,setListaAcessosEstados] = useState([]);
-  const [listaAcessosOperadoras,setListaAcessosOperadoras] = useState([]);
-  const [listaVelocidade,setListaVelocidade] = useState([]);
-      useEffect ( () => {
-        
-          const fetchData = async () =>{
-            //
-            const estados = collection(db, 'acessosPorEstados');
-            const SnapshotEstados = await getDocs(estados);
-            let novaListaEstados = [...listaAcessosEstados];
-            SnapshotEstados.forEach(doc => {
-                const obj1 ={estado: doc.data().estado,acesso: doc.data().acessos};
-                novaListaEstados.push(obj1);
-            });
-              //=======================================================//
-            const operadoras = collection(db, 'acessoPorOperadoras');
-            const SnapshotOperadoras = await getDocs(operadoras);
-            let novaListaOperadoras = [...listaAcessosOperadoras];
-            SnapshotOperadoras.forEach(doc => {
-              const obj2 ={banda_larga: doc.data().banda_larga,
-                  celular: doc.data().celular,
-                  operadora: doc.data().operadora,
-                  telefone_fixo: doc.data().telefone_fixo,
-                  tv: doc.data().tv};
-                  novaListaOperadoras.push(obj2);
-              });
-            const velocidade = collection(db, 'velocidadePorRegiao');
-            const SnapshotVelocidade = await getDocs(velocidade);
-            let novaListaVelocidade = [...listaVelocidade];
-            SnapshotVelocidade.forEach(doc => {
-                const obj3 ={mbs: doc.data().mbs,regiao: doc.data().regiao};
-                novaListaVelocidade.push(obj3);
-            });
-            setListaAcessosEstados(novaListaEstados);
-            setListaAcessosOperadoras(novaListaOperadoras);
-            setListaVelocidade(novaListaVelocidade);
-            //console.log(novaLista);
-          }
-          fetchData();
-      },[]);
 
   return (
     <div>
@@ -76,11 +29,11 @@ function App() {
         <BarraMenu/>
         <Routes>
           <Route path="/" element={<PaginaInicial></PaginaInicial>} />
-          <Route path="/acessosPorEstado" element={<AcessosEstado lista={listaAcessosEstados}></AcessosEstado>}/>
+          <Route path="/acessosPorEstado" element={<AcessosEstado></AcessosEstado>}/>
           <Route path="/faleConosco" element={<FaleConosco></FaleConosco>
           }/>
-          <Route path="/acessosPorOperadoras" element={<AcessosOperadoras lista={listaAcessosOperadoras}></AcessosOperadoras>}/>
-          <Route path="/velocidadePorRegiao" element={<VelocidadeRegiao lista={listaVelocidade}></VelocidadeRegiao>}/>
+          <Route path="/acessosPorOperadoras" element={<AcessosOperadoras></AcessosOperadoras>}/>
+          <Route path="/velocidadePorRegiao" element={<VelocidadeRegiao></VelocidadeRegiao>}/>
         </Routes>
         
         <div><MainBg/></div>
